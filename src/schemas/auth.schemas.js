1ec13e4ba5df73a91f7e6a1d73d59c69e828d0d6
José Matheus <XxFULLDLCxX@outlook.com>
2023-08-13 20:46:05 -0300

    fix: Update schema to validate phone number

--- src/schemas/auth.schemas.js
+++ src/schemas/auth.schemas.js
@@ -4,7 +4,7 @@ export const signup_schema = Joi.object({
   name: Joi.string().min(4).max(40).required(),
   email: Joi.string().email().max(128).required(),
   password: Joi.string().min(3).required(),
-  phone: Joi.string().min(10).max(11).pattern(/^\d+$/).required(),
+  phone: Joi.string().min(7).max(15).pattern(/^\+?\d{1,14}$/).required(),
   CPF: Joi.string().length(11).pattern(/^\d+$/).required(),
 })
 
